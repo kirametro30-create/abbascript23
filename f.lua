d28938adede4cff1436a41753d3c3b0f8b24b828
kirametro30-create <kirametro3.0@gmail.com>
2025-11-27 15:22:28 +0500

    Create f.lua

---
+++ f.lua
@@ -0,0 +1,13 @@
+local char = game:GetService("Players").LocalPlayer.Character
+char.LeftFoot.MeshId = "http://www.roblox.com/asset/?id=902942077"
+char.LeftFoot.Transparency = 1
+char.LeftLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942101"
+char.LeftLowerLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+char.LeftUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942082"
+char.LeftUpperLeg.TextureID = "http://roblox.com/asset/?id=902842271"
+char.RightFoot.MeshId = "http://www.roblox.com/asset/?id=902942089"
+char.RightFoot.Transparency = 1
+char.RightLowerLeg.MeshId = "http://www.roblox.com/asset/?id=902942093"
+char.RightLowerLeg.Transparency = 1
+char.RightUpperLeg.MeshId = "http://www.roblox.com/asset/?id=902942096"
+char.RightUpperLeg.TextureID = "http://roblox.com/asset/?id=902843398"
